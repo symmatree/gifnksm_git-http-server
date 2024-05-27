393ccbc0ba62a38089c80cf03a7d07a4b89f32bf
Seth Porter <symmetry@pobox.com>
2024-05-27 12:54:59 -0400

    Recreate nginx user and group with chosen GID / UID

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,6 +7,33 @@ readonly HTTP_GROUP=nginx
 
 readonly FCGI_SOCKET=/var/run/fcgiwrap.sock
 readonly FCGI_PROGRAM=/usr/bin/fcgiwrap
+# Defaults from
+# https://github.com/nginxinc/docker-nginx/blob/master/mainline/alpine-slim/Dockerfile
+readonly GID="${OVERRIDE_GID:-101}"
+readonly UID="${OVERRIDE_GID:-101}"
+
+if [ -z "${BASE_URL+x}" ]; then
+  echo 'using default base_url'
+else
+  echo "overriding base_url with ${BASE_URL}"
+  # We need to set a variable with a literal dollar-sign to an interpolated
+  # variable which needs to be double-quoted. So:
+  echo 'our $base_url = "'"${BASE_URL}"'";' >> /etc/gitweb.conf 
+fi
+
+# We could do this in Dockerfile but it's as easy to do at runtime.
+
+# Only user in `users`
+deluser guest
+# We don't have "allow duplicate" so get rid of users first.
+delgroup users
+
+# Get rid of existing user (will also delete group)
+deluser nginx
+echo "recreating user nginx with uid:gid ${UID}:${GID}"
+# -D -H: No password, no homedir.
+addgroup -g "${GID}" nginx
+adduser -u "${UID}" -D -H -g "nginx within docker" nginx nginx
 
 env -i /usr/bin/spawn-fcgi \
   -s "${FCGI_SOCKET}" \
@@ -16,8 +43,10 @@ env -i /usr/bin/spawn-fcgi \
   -U "${HTTP_USER}" \
   -G "${HTTP_GROUP}" \
   -- \
-  "${FCGI_PROGRAM}"
+  "${FCGI_PROGRAM}" \
+  -f
 
-sudo -u nginx /usr/local/bin/repomng &
+doas -u nginx /usr/local/bin/repomng &
 
+# This runs /docker-entrypoint.d hooks, then the command.
 exec /docker-entrypoint.sh "$@"
